8384cba0ef33c3264a9f0edd7ac13b30854ba72d
gull2112 <KJY112112@NAVER.COM>
2024-04-05 15:24:43 +0900

    5주차 과제
    과제 완료

---
+++ 5주차 과제.py
@@ -0,0 +1,13 @@
+point  = int(input("점수를 입략하세요 : "))
+
+if point >= 71 :
+    print("A")
+
+elif point >= 41 :
+    print("B")
+
+elif point >= 11 :
+    print("C")
+
+elif point <= 10 :
+    print("D")
